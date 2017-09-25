088e8471257a45c6da1dac8cb11cf85cf80e1f43
Mikkel Sørensen <Mikk4211@edu.easj.dk>
2017-09-25 16:32:26 +0200

    Add files via upload

---
+++ Opgave 9.1.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleRectangle Rektangel1 = new SimpleRectangle();
+        System.out.println("The area of the rectangle with height and width " + Rektangel1.  )
+
+
+
+    }
+}
