c9865227ca911c7b2ce5fe451d190a4f37410b58
Mikkel Sørensen <Mikk4211@edu.easj.dk>
2017-11-15 07:42:48 +0100

    Update Aktie.java

--- Opgave 9.2/Aktie.java
+++ Opgave 9.2/Aktie.java
@@ -1,19 +1,25 @@
+/* Lavet af Mikkel Sørensen
+Mikk4211@edu.easj.dk
+EASJ Næstved, DAT 1 */
+
 package com.company;
 
 public class Aktie {
-
+    //Deklarerer variabler, uinstancierede.
     String symbol;
     String navn;
 
     double tidlPris;
     double nuvPris;
 
+    //Arg constructor
     Aktie(String nySymbol, String nyNavn) {
 
         symbol = nySymbol;
         navn = nyNavn;
     }
-
+    
+    //Får differencen i %
     double getDiff() {
 
         return ((nuvPris - tidlPris) / tidlPris) * 100;
